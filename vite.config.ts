da3a5f4a80e2735dc494a0bf8e2463e443c9e8ad
Eirik Vågeskar <playground.gate@protonmail.com>
2023-12-19 08:16:22 +0100

    Externalize dependencies in dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from "vite";
 import electron from "vite-plugin-electron";
+import { dependencies as rootDependencies } from "./package.json";
+const packagesThatOnlyWorkIfLeftOutsideOfBundle = Object.keys(rootDependencies);
 
 export default defineConfig({
   plugins: electron({
     entry: "./main.ts",
+    vite: {
+      build: {
+        rollupOptions: { external: packagesThatOnlyWorkIfLeftOutsideOfBundle },
+      },
+    },
   }),
 });
